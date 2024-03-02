f1d4516cd1fd56a50fb051c481f39fda7ba46e5e
Pablo Opazo <paoc@Pablos-MacBook-Pro.local>
2024-03-02 12:51:23 -0700

    Init structure

--- docker-compose-watcher.py
+++ docker-compose-watcher.py
@@ -9,18 +9,27 @@ class ChangeHandler(FileSystemEventHandler):
     """Handles the file change events."""
     def on_any_event(self, event):
         print(f"Event detected: {event.src_path}, {event.event_type}")
-        print("Restarting Docker Compose services...")
+        print("Building Docker Compose services...")
         try:
             # Set the current working directory to the script's directory
             os.chdir(os.path.dirname(os.path.abspath(__file__)))
+            # Build the Docker images
+            subprocess.run(["docker-compose", "build"], check=True)
+            print("Docker Compose services built successfully.")
+            
+            print("Restarting Docker Compose services...")
+            # Stop, remove containers and their volumes
             subprocess.run(["docker-compose", "down", "-v"], check=True)
+            # Start the containers in detached mode
             subprocess.run(["docker-compose", "up", "-d"], check=True)
             print("Docker Compose services restarted successfully.")
+            
             print("Fetching Docker Compose logs...")
             command = "docker-compose logs | awk -F' [|] ' '{print \"{\\\"container_name\\\":\\\"\" $1 \"\\\", \" substr($2, 2) }' | jq ."
             subprocess.run(command, shell=True, check=True, executable="/bin/bash")
         except subprocess.CalledProcessError as e:
-            print(f"Error occurred while restarting Docker Compose services or fetching logs: {e}")
+            print(f"Error occurred while building or restarting Docker Compose services or fetching logs: {e}")
+
 
 def main():
     path = os.path.dirname(os.path.abspath(__file__))  # Watch the script's directory
---
+++ src/webapp/main.py
@@ -0,0 +1,15 @@
+import json
+
+def hello_world():
+    message = "Hello, World! for the webapp!"
+    data = {
+        "message": message
+    }
+    json_data = json.dumps(data)
+    return json_data
+
+def main():
+    print(hello_world())
+
+if __name__ == "__main__":
+    main()
