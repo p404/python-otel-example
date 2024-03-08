847054d9c2b56cc309a1f8fb10aff39e167d820e
Pablo Opazo <paoc@Pablos-MacBook-Pro.local>
2024-03-07 17:11:03 -0700

    Working tracer

--- src/webapp/main.py
+++ src/webapp/main.py
@@ -1,12 +1,26 @@
 import json
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+# OTEL Traces initialization
+provider = TracerProvider()
+otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True)
+batch_processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(batch_processor)
+
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer("my.tracer.something")
 
 def hello_world():
-    message = "Hello, World! for the webapp!"
-    data = {
-        "message": message
-    }
-    json_data = json.dumps(data)
-    return json_data
+    with tracer.start_as_current_span("hellow-world-span") as span:
+        message = "Hello, World! for the webapp!"
+        data = {
+            "message": message
+        }
+        json_data = json.dumps(data)
+        return json_data
 
 def main():
     print(hello_world())
